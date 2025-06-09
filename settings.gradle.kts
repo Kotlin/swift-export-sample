4b95f2a59790b9e51cf090c1806a1a9749fdf76a
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-06-09 17:04:07 +0300

    Bump Kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/") {}
+        maven("https://packages.jetbrains.team/maven/p/kt/bootstrap/") {}
         gradlePluginPortal()
         mavenCentral()
 
@@ -12,7 +12,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/") {}
+        maven("https://packages.jetbrains.team/maven/p/kt/bootstrap/") {}
         mavenCentral()
     }
 }
